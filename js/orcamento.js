3d45d441f803fa314d85466a3a0c6a9b934ea334
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2023-03-04 12:46:45 -0300

    Refatoração do código JS da página de orçamentos

--- js/orcamento.js
+++ js/orcamento.js
@@ -1,60 +1,55 @@
-const inputList = Array.from(document.querySelectorAll('.menu-opcoes input'));
+const menuInputs = Array.from(document.querySelectorAll('.menu-1 input'));
+const productInputs = Array.from(document.querySelectorAll('.menu-bikcraft input'));
+const parametros = Array.from(new URLSearchParams(location.search));
+let lastProduct;
 
-function bikeOuSeguro() {
-    if (inputList[inputList.indexOf(document.querySelector(`input#bikcraft`))].checked == true) {
-        document.querySelector(".menu-seguro").style.display = "none";
-        document.querySelector(".menu-bikcraft").style.display = "grid";
-    } else {
-        document.querySelector(".menu-seguro").style.display = "grid";
-        document.querySelector(".menu-bikcraft").style.display = "none";
+bikeOuSeguro();
+
+function bikeOuSeguro(event) {
+    if (event !== undefined) {
+        if (event.target.getAttribute('data-menu') == 1) {
+            document.querySelector('.menu-bikcraft').style.display = 'grid';
+            document.querySelector('.menu-seguro').style.display = 'none';
+        } else {
+            document.querySelector('.menu-seguro').style.display = 'grid';
+            document.querySelector('.menu-bikcraft').style.display = 'none';
+        }
+    } else if (event === undefined && parametros.length !== 0) {
+        const inputToCheck = document.querySelector(`.menu-1 input#${parametros[0][1]}`);
+        inputToCheck.checked = 'true';
+        document.querySelector(`.menu-${parametros[0][1]}`).style.display = 'grid';
+        exibirDetalhes();
     }
 }
 
-const nimbusInput = document.querySelector("#nimbus");
-const nimbusDetail = document.querySelector(".detalhes-produto.nimbus");
-const magicInput = document.querySelector("#magic");
-const magicDetail = document.querySelector(".detalhes-produto.magic");
-const nebulaInput = document.querySelector("#nebula");
-const nebulaDetail = document.querySelector(".detalhes-produto.nebula");
+function exibirDetalhes(event) {
+    let detailsToBeOpen;
+    if (event !== undefined) {
+        detailsToBeOpen = document.querySelector(`[data-name=${event.target.getAttribute('value')}]`)
+        detailsToBeOpen.style.display = 'grid';
 
-function exibirDetalhes() {
-    if (nimbusInput.checked == true) {
-        nimbusDetail.style.display = "grid";
-        magicDetail.style.display = "none";
-        nebulaDetail.style.display = "none";
-    } else if (magicInput.checked == true) {
-        magicDetail.style.display = "grid";
-        nimbusDetail.style.display = "none";
-        nebulaDetail.style.display = "none";
+        if (lastProduct !== detailsToBeOpen && lastProduct !== undefined) {
+            lastProduct.style.display = 'none';
+        }
+        lastProduct = detailsToBeOpen;
     } else {
-        nebulaDetail.style.display = "grid";
-        nimbusDetail.style.display = "none";
-        magicDetail.style.display = "none";
+        const inputToCheck = document.querySelector(`.menu-opcoes input#${parametros[1][1]}`);
+        inputToCheck.checked = 'true';
+        if (inputToCheck.getAttribute('data-menu') == 1) {
+            detailsToBeOpen = document.querySelector(`[data-name=${document.querySelector(`.menu-opcoes input#${parametros[1][1]}`).getAttribute('value')}]`)
+            detailsToBeOpen.style.display = 'grid';
+            lastProduct = detailsToBeOpen;
+        }
     }
 }
 
-/* HABILITAR INPUTS CONFORME SELEÇÕES */
-
-function observarInputsMenu(item) {
+function firstListener(item) {
     item.addEventListener('click', bikeOuSeguro);
 }
 
-function observarBikeSelecionada(item) {
+function secondListener(item) {
     item.addEventListener('click', exibirDetalhes);
 }
 
-Array.from(document.querySelectorAll('.menu-1 input')).forEach(observarInputsMenu);
-
-Array.from(document.querySelectorAll('.menu-bikcraft input')).forEach(observarBikeSelecionada);
-
-/* ATIVAR PRODUTO QUE FOI SELECIONADO NAS TELAS ANTERIORES */
-const parametros = Array.from(new URLSearchParams(location.search));
-
-function ativarProduto() {
-    document.querySelector(`.menu-1 input#${parametros[0][1]}`).checked = 'true';
-    bikeOuSeguro();
-    document.querySelector(`.menu-${parametros[0][1]} input#${parametros[1][1]}`).checked = 'true';
-    exibirDetalhes();
-}
-
-parametros.forEach(ativarProduto);
+menuInputs.forEach(firstListener);
+productInputs.forEach(secondListener);
